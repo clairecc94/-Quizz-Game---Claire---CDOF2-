472a1ba264feb4b693c981461eddb505cdbcdef1
Claire CUCHE <claire.cuche@edu.devinci.fr>
2025-01-13 10:46:42 +0100

    Creation of files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='quizz-game',
+    version='1.0',
+    author='Claire',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
